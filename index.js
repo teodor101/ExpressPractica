9fca743a2138a29258cbab4e22ccdb15ca035dd7
teodor101 <curuiateodor@gmail.com>
2026-01-14 21:17:31 +0100

    DB implementada

--- index.js
+++ index.js
@@ -1,16 +1,36 @@
 import express from "express";
 import productosRouter from "./routes/productos.js";
 import saludosRouter from "./routes/saludos.js";
+import pkg from "pg";
 
-const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000
+const app = express() //inicializamos express
+
+
+const { Pool } = pkg;
 
 app.use(express.json());
-
+app.use("/saludos",saludosRouter)
 app.use("/productos", productosRouter);
-app.use("/saludos", saludosRouter);
+
+const db = new Pool({
+  host: "db",
+  user: "postgres",
+  password: "123456",
+  database: "postgres",
+  port: 5432,
+});
+
+db.connect()
+  .then(() => {
+    console.log("Conectado a PostgreSQL");
+  })
+  .catch((err) => {
+    console.error("Error de conexión:", err);
+  });
+
 
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+    console.log(`Server running on port ${PORT}`)
+})
--- routes/productos.js
+++ routes/productos.js
@@ -3,8 +3,6 @@ import ProductoController from "../controllers/ProductoController.js";
 
 const router = Router();
 
-
-
 router.get("/productos", ProductoController.getProductos);
 
 router.post("/productos" , ProductoController.crearProducto);
